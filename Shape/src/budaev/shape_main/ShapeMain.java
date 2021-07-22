a66d472d22bd584898260a8ec0dbb257207896b6
BatoBudaev <Axl206311>
2021-07-22 19:46:24 +0700

    Shape.iml пуш

--- Shape/src/budaev/shape_main/ShapeMain.java
+++ Shape/src/budaev/shape_main/ShapeMain.java
@@ -3,14 +3,14 @@ package budaev.shape_main;
 import budaev.shape.*;
 
 public class ShapeMain {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         Shape[] shapesArray = new Shape[5];
         shapesArray[0] = new Square(2);
         shapesArray[1] = new Rectangle(5, 10);
         shapesArray[2] = new Triangle(1, 2, 3, 7, 5, 10);
         shapesArray[3] = new Circle(8);
         shapesArray[4] = new Square(5);
-        
+
     }
 
    /*  public void getMaxArea (Shape[] shapesArray){
