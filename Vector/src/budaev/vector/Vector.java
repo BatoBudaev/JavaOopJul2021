33ad64a9040c98e75f6730fe4f5183e0f1b443b3
BatoBudaev <Axl206311>
2021-08-11 00:10:24 +0700

    Добавлена перая часть задачи вектор.

--- Vector/src/budaev/vector/Vector.java
+++ Vector/src/budaev/vector/Vector.java
@@ -1,10 +1,9 @@
 package budaev.vector;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 
 public class Vector {
-    private final double[] vector;
+    private double[] vector;
 
     public Vector(int n) throws IllegalArgumentException {
         if (n <= 0) {
@@ -24,7 +23,11 @@ public class Vector {
         System.arraycopy(array, 0, vector, 0, array.length);
     }
 
-    public Vector(int n, double[] array) {
+    public Vector(int n, double[] array) throws IllegalArgumentException {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n <= 0");
+        }
+
         vector = new double[n];
 
         for (int i = 0; i < n; i++) {
@@ -44,5 +47,75 @@ public class Vector {
     public String toString() {
         String line = Arrays.toString(vector);
         return "{ " + line.substring(1, line.length() - 1) + " }";
+    }
+
+    public void add(double e) {
+        double[] tempArray = Arrays.copyOf(vector, vector.length + 1);
+        tempArray[vector.length] = e;
+        vector = tempArray;
+    }
+
+    private void getEqualLengthVectors(Vector vector1, Vector vector2) {
+        if (vector1.getSize() == vector2.getSize()) {
+            return;
+        }
+
+        double[] array;
+
+        if (vector1.getSize() > vector2.getSize()) {
+            array = Arrays.copyOf(vector2.vector, vector1.getSize());
+            vector2.vector = array;
+        } else {
+            array = Arrays.copyOf(vector1.vector, vector2.getSize());
+            vector1.vector = array;
+        }
+    }
+
+    public void addition(Vector otherVector) {
+        if (vector.length != otherVector.getSize()) {
+            getEqualLengthVectors(this, otherVector);
+        }
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] += otherVector.vector[i];
+        }
+    }
+
+    public void subtraction(Vector otherVector) {
+        if (vector.length != otherVector.getSize()) {
+            getEqualLengthVectors(this, otherVector);
+        }
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] -= otherVector.vector[i];
+        }
+    }
+
+    public void multiplyByScalar(double scalar) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] *= scalar;
+        }
+    }
+
+    public void expand() {
+        multiplyByScalar(-1);
+    }
+
+    public double getLength() {
+        double sum = 0;
+
+        for (double e : vector) {
+            sum += e * e;
+        }
+
+        return Math.sqrt(sum);
+    }
+
+    public double getComponent(int index) {
+        return vector[index];
+    }
+
+    public void setComponent(double value, int index) {
+        vector[index] = value;
     }
 }
--- Vector/src/budaev/vector_main/VectorMain.java
+++ Vector/src/budaev/vector_main/VectorMain.java
@@ -6,13 +6,18 @@ public class VectorMain {
     public static void main(String[] args) {
         Vector vector = new Vector(5);
 
-        double[] arr = {1.2, 3};
+        double[] arr = {1, 3, 7};
 
         Vector vector2 = new Vector(arr);
 
-        Vector vector3 = new Vector(10,arr);
-        System.out.println(vector3);
+        double[] arr2 = {5, 4};
+        Vector vector3 = new Vector(2, arr2);
 
+        vector2.addition(vector3);
 
+        vector2.add(10);
+        vector2.expand();
+
+        System.out.println(vector2);
     }
 }
