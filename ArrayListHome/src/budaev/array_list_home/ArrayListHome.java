309d444749232c7e4939da14eed4820737991723
BatoBudaev <Axl206311>
2021-08-21 00:39:09 +0700

    Исправлена задача ArrayListHome.

--- ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
@@ -1,39 +1,49 @@
 package budaev.array_list_home;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome/src/input.txt"))) {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("ArrayListHome/src/input.txt"))) {
             ArrayList<String> linesList = new ArrayList<>();
+            String line;
 
-            while (scanner.hasNextLine()) {
-                linesList.add(scanner.nextLine());
+            while ((line = reader.readLine()) != null) {
+                linesList.add(line);
             }
 
-            for (String line : linesList) {
-                System.out.println(line);
+            for (String lineForPrint : linesList) {
+                System.out.println(lineForPrint);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         //2
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 3, 5, 6, 8, 9));
-        numbersList.removeIf(number -> number % 2 == 0);
+        Iterator<Integer> iterator = numbersList.iterator();
 
-        System.out.println(numbersList);
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
 
-        //3
-        ArrayList<Integer> numbersList2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
-        ArrayList<Integer> newNumbersList = new ArrayList<>();
-
-        for (Integer number : numbersList2) {
-            if (!newNumbersList.contains(number)) {
-                newNumbersList.add(number);
+            if (number % 2 == 0) {
+                iterator.remove();
             }
         }
 
-        System.out.println(newNumbersList);
+        System.out.println("Список без четных чисел: " + numbersList);
+
+        //3
+        ArrayList<Integer> numbersList2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
+        ArrayList<Integer> numbersListWithoutDuplicates = new ArrayList<>(numbersList2.size());
+
+        for (Integer number : numbersList2) {
+            if (!numbersListWithoutDuplicates.contains(number)) {
+                numbersListWithoutDuplicates.add(number);
+            }
+        }
+
+        System.out.println("Список чисел без повторений: " + numbersListWithoutDuplicates);
     }
 }
