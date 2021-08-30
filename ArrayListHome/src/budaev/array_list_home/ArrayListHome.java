d4d544044ccc4167211b217cc03669c5a7b694ea
BatoBudaev <Axl206311>
2021-08-31 02:11:27 +0700

    Исправлена задача ArrayListHome.

--- ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class ArrayListHome {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("ArrayListHome/src/input.txt"))) {
             ArrayList<String> linesList = new ArrayList<>();
             String line;
@@ -16,25 +16,23 @@ public class ArrayListHome {
             for (String lineForPrint : linesList) {
                 System.out.println(lineForPrint);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
         }
 
-        //2
-        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 3, 5, 6, 8, 9));
-        Iterator<Integer> iterator = numbersList.iterator();
+        // 2
+        ArrayList<Integer> numbersList1 = new ArrayList<>(Arrays.asList(1, 3, 5, 6, 8, 9));
 
-        while (iterator.hasNext()) {
-            Integer number = iterator.next();
-
-            if (number % 2 == 0) {
-                iterator.remove();
+        for (int i = 0; i < numbersList1.size(); i++) {
+            if (numbersList1.get(i) % 2 == 0) {
+                numbersList1.remove(i);
+                i--;
             }
         }
 
-        System.out.println("Список без четных чисел: " + numbersList);
+        System.out.println("Список без четных чисел: " + numbersList1);
 
-        //3
+        // 3
         ArrayList<Integer> numbersList2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
         ArrayList<Integer> numbersListWithoutDuplicates = new ArrayList<>(numbersList2.size());
 
