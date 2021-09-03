949251c4d3fbfcdc1a275a4a440943c3cad2f15b
BatoBudaev <Axl206311>
2021-09-03 13:06:50 +0700

    Исправлена задача ArrayListHome.

--- ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/budaev/array_list_home/ArrayListHome.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class ArrayListHome {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("ArrayListHome/src/input.txt"))) {
             ArrayList<String> linesList = new ArrayList<>();
             String line;
@@ -18,7 +18,7 @@ public class ArrayListHome {
             }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Ошибка ввода-вывода");
         }
 
